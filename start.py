ece4058ba6e72b47bca3e4db133d3360c9835aaa
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-04-20 09:06:52 +0700

    Add files via upload

--- start.py
+++ start.py
@@ -13,8 +13,9 @@ t_labels = np.array(t_labels)
 
 print_statistics(images, t_images, labels, t_labels)
 
-bias = np.ones((images.shape[0],1))
-t_bias = np.ones((t_images.shape[0],1))
-
-np.hstack(bias, images)
-np.hstack(t_bias, t_images)
+(m,n) = images.shape
+bias = np.ones((m,1))
+images = np.hstack((bias,images))
+(m2,n2) = t_images.shape
+t_bias = np.ones((m2,1))
+t_images = np.hstack((t_bias,t_images))
