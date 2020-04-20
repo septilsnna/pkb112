3a176ff1e0d455396993db2247b70cda986ccb9b
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-04-20 08:22:07 +0700

    Add files via upload

--- start.py
+++ start.py
@@ -1,4 +1,14 @@
 from mnist import MNIST
+import numpy as np
+from lib import *
+
 mndata = MNIST('.data')
 images, labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
+
+images = np.array(images)
+t_images = np.array(t_images)
+labels = np.array(labels)
+t_labels = np.array(t_labels)
+
+print_statistics(images, t_images, labels, t_labels)
