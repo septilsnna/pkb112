a72ae01784492c8976142cd349c1fd908e15ebc1
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-04-20 08:21:26 +0700

    Add files via upload

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from mnist import MNIST
 import numpy as np
+from lib import *
 
 mndata = MNIST('.data')
 images, labels = mndata.load_training()
@@ -10,15 +11,4 @@ t_images = np.array(t_images)
 labels = np.array(labels)
 t_labels = np.array(t_labels)
 
-#number of training dataset
-len(labels)
-#number of test dataset
-len(t_labels)
-#number of class
-len(np.unique(labels))
-#number of instances per class on training
-for i in np.unique(labels):
-    np.sum(labels==i)
-#number of instances per class on test dataset
-for i in np.unique(t_labels):
-    np.sum(t_labels==i)
+print_statistics(images, t_images, labels, t_labels)
